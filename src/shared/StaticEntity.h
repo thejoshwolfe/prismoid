4afe863c69c63f94c440a153f8b340ae80fe7cab
Josh Wolfe <thejoshwolfe@gmail.com>
2012-09-03 05:40:30 -0700

    fixing static entity prismoid generation

--- src/shared/StaticEntity.h
+++ src/shared/StaticEntity.h
@@ -9,6 +9,10 @@ public:
     StaticEntity(const Vector2 & center, const Vector2 & size) :
         Entity(center, size)
     {
+        std::vector<Vector2> here;
+        Util::makeRectangle(&here, center, size);
+        for (int i = 0; i < (int)here.size(); i++)
+            bounding_prismoid.addEdge(here[i], here[i]);
     }
 
     virtual const Vector2& getVelocity() const { return zero; }
